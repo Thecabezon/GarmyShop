437da3268522ece15ebef7aee63e211b5daea782
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-05-20 12:22:47 -0500

    Implementación de filtrado de productos por slug de categoría

--- user-backend/user-backend/src/main/java/com/garmyshop/user_backend/repository/CategoriaRepository.java
+++ user-backend/user-backend/src/main/java/com/garmyshop/user_backend/repository/CategoriaRepository.java
@@ -3,7 +3,9 @@ package com.garmyshop.user_backend.repository;
 import com.garmyshop.user_backend.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+    Optional<Categoria> findBySlug(String id);
 }
--- user-backend/user-backend/src/main/java/com/garmyshop/user_backend/repository/ProductoRepository.java
+++ user-backend/user-backend/src/main/java/com/garmyshop/user_backend/repository/ProductoRepository.java
@@ -1,8 +1,11 @@
 package com.garmyshop.user_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.garmyshop.user_backend.entity.Categoria;
 import com.garmyshop.user_backend.entity.Producto;
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByCategoria(Categoria categoria);
 
 }
