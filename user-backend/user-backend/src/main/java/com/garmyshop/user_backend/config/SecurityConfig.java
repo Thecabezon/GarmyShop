fb2ae4235d6ed62ab18026d7f685363d03c02c08
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-07-08 17:33:06 -0500

    Documentacion swagger

--- user-backend/user-backend/src/main/java/com/garmyshop/user_backend/config/SecurityConfig.java
+++ user-backend/user-backend/src/main/java/com/garmyshop/user_backend/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.POST, "/api/contacto").permitAll()
                     .requestMatchers("/login/oauth2/code/**").permitAll()
                     .requestMatchers("/oauth2/**").permitAll()
-                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/swagger-ui.html").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/categorias/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/marcas/**").permitAll()
